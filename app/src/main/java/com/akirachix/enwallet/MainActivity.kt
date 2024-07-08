aaeeec109f5ac1e76929a1bfc597fddda63f8490
Wachira-joy <wachiraglorywairimu@gmail.com>
2024-07-08 11:12:13 +0300

    Second commit

--- app/src/main/java/com/akirachix/enwallet/MainActivity.kt
+++ app/src/main/java/com/akirachix/enwallet/MainActivity.kt
@@ -11,8 +11,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-     binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
     }
 }
