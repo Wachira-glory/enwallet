f3fbc05faac57734b31d4e9098836d72b52914ac
Wachira-joy <wachiraglorywairimu@gmail.com>
2024-07-08 09:11:04 +0300

    first commit

---
+++ app/src/main/java/com/akirachix/enwallet/EnwalletActivity.kt
@@ -0,0 +1,16 @@
+package com.akirachix.enwallet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.akirachix.enwallet.databinding.ItemListBinding
+
+class EnwalletActivity : AppCompatActivity() {
+    lateinit var binding: EnwalletActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
